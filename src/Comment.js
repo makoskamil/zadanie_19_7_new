655b8ec5fa8f4151e426d6bc86ca80ccc84aff62
Kamil Makoś <makos.kamil@o2.pl>
2018-07-18 20:44:48 +0200

    change

--- src/Comment.js
+++ src/Comment.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {ADD_COMMENT} from './actions.js';
 
-const Comment = ({text, votes}) => <li>{text} <span>votes: {votes}</span></li>;
+const Comment = ({text, votes, id, thumbUpComment}) =>
+  <li>
+    {text} <span>votes: {votes}</span> <button onClick={() => thumbUpComment(id)}>Thumb up</button>
+  </li>;
 
 export default Comment;
--- src/CommentContainer.js
+++ src/CommentContainer.js
@@ -3,7 +3,10 @@ import Comment from './Comment';
 import {thumbUpComment} from './index'
 
 const mapDispatchToProps = dispatch => ({
-  thumbUpComment: (id) => dispatch(thumbUpComment(id))
+  thumbUpComment: (id) => dispatch(thumbUpComment(id)),
+  thumbDownComment: (id) => dispatch(thumbDownComment(id)),
+  removeComment: (id) => dispatch(removeComment(id)),
+  editComment: (id, text) => dispatch(editComment(id, text))
 });
 
 export default connect(null, mapDispatchToProps)(Comment);
